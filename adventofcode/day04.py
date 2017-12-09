33014ccf99ac882e9adcf65d0020a50de01360c1
Brian Svoboda <svobodb@email.arizona.edu>
2017-12-09 14:54:56 -0700

    working Day 4

--- adventofcode/day04.py
+++ adventofcode/day04.py
@@ -1,13 +1,7 @@
 #!/usr/bin/env python3
 
+from collections import Counter
 import numpy as np
-
-
-PUZZLE_INPUT = """
-aa bb cc dd ee
-aa bb cc dd aa
-aa bb cc aaa
-"""
 
 
 def parse_input(puzzle_input):
@@ -17,14 +11,27 @@ def parse_input(puzzle_input):
     ]
 
 
-def valid_passphrase(phrase):
+def validate_unique(phrase):
     return len(phrase) == len(np.unique(phrase))
 
 
+def validate_anagram(phrase):
+    words = [Counter(w) for w in phrase]
+    while words:
+        w = words.pop()
+        if w in words:
+            return False
+    else:
+        return True
+
+
 def solve():
-    phrases = parse_input(PUZZLE_INPUT)
-    good_phrases = sum(valid_passphrase(phrase) for phrase in phrases)
+    with open('pi04.txt', 'r') as f:
+        phrases = parse_input(f.read())
+    unique_phrases = sum(validate_unique(phrase) for phrase in phrases)
+    nonanagram_phrases = sum(validate_anagram(phrase) for phrase in phrases)
     print('-- total phrases :', len(phrases))
-    print('-- good phrases  :', good_phrases)
+    print('-- unique phrases :', unique_phrases)
+    print('-- non-anagram phrases :', nonanagram_phrases)
 
 
