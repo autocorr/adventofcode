093dd7371a0a8ddc3d33a5ee83552a8f727bc9af
Brian Svoboda <svobodb@email.arizona.edu>
2017-12-09 23:18:47 -0700

    working Day 3

--- adventofcode/day03.py
+++ adventofcode/day03.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 # -> Spiral as ccw
 #
 #   65 64 63 62 61 60 59 58 57
@@ -11,29 +10,34 @@
 #   71 42 21 22 23 24 25 26 51
 #   72 43 44 45 46 47 48 49 50
 #   73 74 75 76 77 78 79 80 81
+#                4  5  6  7  8
 #
-# -> functions by "radius" R in straight lines
-#   lower-right :  n = 4 * R**2 -  4 * R + 1
-#   down        :  n = 4 * R**2 -  5 * R + 2
-#   lower-left  :  n = 4 * R**2 -  6 * R + 3
-#   left        :  n = 4 * R**2 -  7 * R + 4
-#   upper-left  :  n = 4 * R**2 -  8 * R + 5
-#   up          :  n = 4 * R**2 -  9 * R + 6
-#   upper-right :  n = 4 * R**2 - 10 * R + 7
-#   right       :  n = 4 * R**2 - 11 * R + 8
+# -> lower-right diagonal increases as square:
+#   R = (2 * n + 1)**2
+#   n = (sqrt(R) - 1) / 2
 #
-# The lower-right diagonal is the largest number before the radius increases.
-# The manhatten distance to the center is symmetric by the four cardinal
-# directions. Can use modulo 4 based on the number of values in an R to R+1
-# interval.
+# The manhatten distance to the center is symmetric by
+# the four cardinal directions.
+# After googling the first few terms, the solution to the second
+# part can be found on OEIS:
+#     https://oeis.org/A141481/b141481.txt
+
+import numpy as np
 
 
-PUZZLE_INPUT = 64
+def manhattan_distance(N):
+    R = int((np.sqrt(N) - 1) / 2) + 1
+    n_hi = (2 * R + 1)**2
+    n_lo = (2 * (R - 1) + 1)**2
+    perim = np.arange(n_lo + 1, n_hi + 1)
+    cards = R + n_lo + (np.arange(4) * (n_hi - n_lo)) // 4
+    offset = min(abs(cards - N))
+    return offset + R
+
 
 def solve():
-    # radius -> solve lower-right, then round down
-    # offset = (value - rad - 1) % 4
-    # dist = radius + offset
-    print('-- :', )
+    #N = 64
+    N = 325489
+    print('-- manhattan distance :', manhattan_distance(N))
 
 
