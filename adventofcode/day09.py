41834f11cc0df846bda80a82555bb9284c7eba33
Brian Svoboda <svobodb@email.arizona.edu>
2017-12-10 12:16:34 -0700

    add stack based solution

--- adventofcode/day09.py
+++ adventofcode/day09.py
@@ -1,64 +1,88 @@
 #!/usr/bin/env python3
 
+from collections import deque
+
 
 def cancel(stream):
-    cancelled = False
-    for s in stream:
-        if cancelled:
-            cancelled = False
+    cancelling = False
+    for c in stream:
+        if cancelling:
+            cancelling = False
             continue
-        elif s == '!':
-            cancelled = True
+        elif c == '!':
+            cancelling = True
             continue
         else:
-            yield s
+            yield c
 
 
 def collect(stream):
     collecting = False
-    for s in stream:
+    for c in stream:
         if collecting:
-            if s == '>':
+            if c == '>':
                 collecting = False
             continue
-        elif s == '<':
+        elif c == '<':
             collecting = True
             continue
         else:
-            yield s
+            yield c
 
 
 def count_score(stream):
-    frame, score = 0, 0
-    for s in stream:
-        if s == '{':
-            frame += 1
-        elif s == '}':
-            score += frame
-            frame -= 1
+    depth, score = 0, 0
+    for c in stream:
+        if c == '{':
+            depth += 1
+        elif c == '}':
+            score += depth
+            depth -= 1
     return score
 
 
 def count_garbage(stream):
-    total = 0
-    collecting = False
-    for s in stream:
+    total, collecting = 0, False
+    for c in stream:
         if collecting:
-            if s == '>':
+            if c == '>':
                 collecting = False
             else:
                 total += 1
             continue
-        elif s == '<':
+        elif c == '<':
             collecting = True
     return total
+
+
+def stack_solution(string):
+    stack = deque(string)
+    score, depth, garbage, collecting = 0, 0, 0, False
+    while stack:
+        c = stack.popleft()
+        if c == '!':
+            stack.popleft()
+        elif collecting and c != '>':
+            garbage += 1
+        elif c == '<':
+            collecting = True
+        elif c == '>':
+            collecting = False
+        elif c == '{':
+            depth += 1
+        elif c == '}':
+            score += depth
+            depth -= 1
+    return score, garbage
 
 
 def solve():
     with open('pi09.txt', 'r') as f:
         stream = f.read().strip()
     #stream = '<{o"i!a,<{i<a>'
-    print('-- curly score :', count_score(collect(cancel(stream))))
-    print('-- total garbage :', count_garbage(cancel(stream)))
+    score = count_score(collect(cancel(stream)))
+    garbage = count_garbage(cancel(stream))
+    print('-- iter based :', score, garbage)
+    print('-- stack based :', *stack_solution(stream))
 
 
